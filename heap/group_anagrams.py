1ec64a263c667efe8f2a056a0c58bfb301de2900
st940 <takumi.shimosakai@proxima-ai-tech.com>
2024-03-24 10:56:19 +0900

    fix: 返り値の型をview objectでなくlistにする

--- heap/group_anagrams.py
+++ heap/group_anagrams.py
@@ -1,10 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
-        temp_dict = {}
+        sorted_word_to_words = {}
         for word in strs:
             sorted_word = ''.join(sorted(word))
-            if sorted_word not in temp_dict:
-                temp_dict[sorted_word] = []
-            temp_dict[sorted_word].append(word)
-        return temp_dict.values()
+            if sorted_word not in sorted_word_to_words:
+                sorted_word_to_words[sorted_word] = []
+            sorted_word_to_words[sorted_word].append(word)
+        return list(sorted_word_to_words.values())
